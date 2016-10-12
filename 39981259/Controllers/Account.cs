38f3172bde8b638c3711d6e842256f1b3132ef41
v-fexue <v-fexue@V-FEXUE-W10>
2016-10-12 14:15:24 +0800

    Add project files.

---
+++ 39981259/Controllers/Account.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _39981259.Controllers
+{
+    public class AccountController: Controller
+    {
+        public async Task Login()
+        {
+            
+            await HttpContext.Authentication.ChallengeAsync(new AuthenticationProperties() { RedirectUri = "/" });
+        }
+
+        public async Task LogOff()
+        {
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                await HttpContext.Authentication.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme, new AuthenticationProperties() { RedirectUri = "/" });
+
+                HttpContext.Response.Redirect("/");
+            }
+        }
+    }
+}
